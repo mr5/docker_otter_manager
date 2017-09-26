7b07c2e9c7a17a930bbcda0232b8ad7b51ea2666
mr5 <u@mr-5.cn>
2017-09-26 17:20:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## docker_otter_manager
+docker for otter_manager
+
+## 环境变量
+
+* DOMAIN_NAME: 域名
+* PORT: 端口号
+* DB_HOST: 数据库地址
+* DB_PORT: [可选] 数据库端口号，默认值为 `3306`
+* DB_NAME: [可选] 数据库名称，默认值为 `otter`
+* DB_USER: 数据库用户名
+* DB_PASSWORD: 数据库密码
+* MAX_MEN: [可选] 进程最大可用内存，默认值为 `3072m`
+* ZK_DEFAULT: 默认的 zookepper ip:port
+* ZK_TIMEOUT: [可选] zookepper session 超时时间，单位是毫秒，默认值 `600000`
+* SMTP_HOST: [可选] 
+* SMTP_USERNAME: [可选] 
+* SMTP_PASSWORD: [可选] 
+* SMTP_PORT: [可选]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if [ -z "$DB_NAME" ]; then
+    DB_NAME=otter
+fi
+if [ -z "$MAX_MEN" ];then
+	MAX_MEN=3072m
+fi
+if [ -z "$DB_PORT" ];then
+	DB_PORT=3306
+fi
+if [ -z "$ZK_TIMEOUT" ];then
+	ZK_TIMEOUT=60000
+fi
+
+echo DOMAIN_NAME: $DOMAIN_NAME
+echo PORT: $PORT
+echo DB_HOST: $DB_HOST
+echo DB_PORT: $DB_PORT
+echo DB_NAME: $DB_NAME
+echo DB_USER: $DB_USER
+echo DB_PASSWORD: $DB_PASSWORD
+echo MAX_MEN: $MAX_MEN
+echo ZK_DEFAULT: $ZK_DEFAULT
+echo ZK_TIMEOUT: $ZK_TIMEOUT
+echo SMTP_HOST: $SMTP_HOST
+echo SMTP_USERNAME: $SMTP_USERNAME
+echo SMTP_PASSWORD: $SMTP_PASSWORD
+echo SMTP_PORT: $SMTP_PORT
+
+sed -i s/"-server -Xms32m -Xmx3072m"/"-server -Xms32m -Xmx$MAX_MEN"/g /opt/otter_manager/bin/startup.sh
+sed -ri "s/(otter.domainName).*/\1 = $DOMAIN_NAME/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.port).*/\1 = $PORT/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.database.driver.url).*/\1 = jdbc:mysql:\/\/$DB_HOST:$DB_PORT\/$DB_NAME/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.database.driver.username).*/\1 = $DB_USER/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.database.driver.password).*/\1 = $DB_PASSWORD/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.zookeeper.cluster.default).*/\1 = $ZK_DEFAULT/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.zookeeper.sessionTimeout).*/\1 = $ZK_TIMEOUT/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.manager.monitor.email.host).*/\1 = $SMTP_HOST/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.manager.monitor.email.username).*/\1 = $SMTP_USERNAME/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.manager.monitor.email.password).*/\1 = $SMTP_PASSWORD/" /opt/otter_manager/conf/otter.properties
+sed -ri "s/(otter.manager.monitor.email.smtp.port).*/\1 = $SMTP_PORT/" /opt/otter_manager/conf/otter.properties
+
+/opt/otter_manager/bin/startup.sh
+tail -f /opt/otter_manager/logs/manager.log & wait
